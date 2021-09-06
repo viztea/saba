d645331d878b4c09763965524cc8d0bd184696a0
melike2d <gino@femboy.email>
2021-09-05 23:42:05 -0700

    :ok_hand: handle playing tracks a bit better.

--- saba-app/src/main/kotlin/lol/saba/app/networking/ActorSession.kt
+++ saba-app/src/main/kotlin/lol/saba/app/networking/ActorSession.kt
@@ -1,5 +1,6 @@
 package lol.saba.app.networking
 
+import kotlinx.coroutines.launch
 import lavaplayer.format.AudioDataFormat
 import lavaplayer.format.AudioPlayerInputStream
 import lavaplayer.format.StandardAudioDataFormats.COMMON_PCM_S16_BE
@@ -7,6 +8,7 @@ import lavaplayer.track.AudioTrack
 import lol.saba.app.SabaApp
 import lol.saba.common.entity.Session
 import lol.saba.common.entity.Track
+import javax.sound.sampled.AudioInputStream
 import javax.sound.sampled.AudioSystem
 import javax.sound.sampled.DataLine
 import javax.sound.sampled.SourceDataLine
@@ -15,36 +17,40 @@ import javax.sound.sampled.SourceDataLine
 class ActorSession(val saba: SabaApp, val guild: Long, val info: Session) {
     val player = saba.players.createPlayer()
     val format: AudioDataFormat = saba.players.configuration.outputFormat
-    val stream = AudioPlayerInputStream.createStream(player, format, 10000L, false)
 
-    private val dataLineInfo = DataLine.Info(SourceDataLine::class.java, stream.format)
-    private val dataLine = AudioSystem.getLine(dataLineInfo) as SourceDataLine
+    var stream: AudioInputStream? = null
+    var dataLine: SourceDataLine? = null
 
     fun shutdown() {
         player.stopTrack()
-        dataLine.flush()
-        dataLine.close()
-        stream.close()
+        stream?.close()
+        dataLine?.close()
     }
 
     fun play(track: Track) {
         val audioTrack = saba.trackUtil.decodeTrack(track.encoded)
         audioTrack.position = track.position
         player.isPaused = track.paused
-        play(track)
+        play(audioTrack)
     }
 
-    fun play(track: AudioTrack) {
+    fun play(track: AudioTrack) = saba.actor.launch {
+        stream?.close()
+        dataLine?.close()
+
+        stream = AudioPlayerInputStream.createStream(player, format, 10000, true)
+        dataLine = AudioSystem.getLine(DataLine.Info(SourceDataLine::class.java, stream?.format)) as SourceDataLine
+
         player.playTrack(track)
 
-        dataLine.open(stream.format)
-        dataLine.start()
+        dataLine!!.open(stream!!.format)
+        dataLine!!.start()
 
         val buffer = ByteArray(COMMON_PCM_S16_BE.maximumChunkSize)
         var chunkSize: Int
 
-        while (stream.read(buffer).also { chunkSize = it } >= 0) {
-            dataLine.write(buffer, 0, chunkSize)
+        while (stream!!.read(buffer).also { chunkSize = it } >= 0) {
+            dataLine!!.write(buffer, 0, chunkSize)
         }
     }
 }
